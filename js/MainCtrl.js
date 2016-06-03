40487ede82b94fc6e91eed7f2b944e6a590c2dda
Matt Higbee <mhigbee@hotmail.com>
2016-06-02 20:57:24 -0600

    saving progress

---
+++ js/MainCtrl.js
@@ -0,0 +1,7 @@
+/**
+ * Created by matthigbee on 6/2/16.
+ */
+angular.module('quoteBook').controller('MainCtrl', function($scope, dataService){
+    $scope.test = "Here's some test";
+
+});
---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by matthigbee on 6/2/16.
+ */
+angular.module('quoteBook', []);
